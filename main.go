a895d81fd3bb378a2f0cff30a25ee650363a7368
Alexey Nikipelau <iloveyoucreator@gmail.com>
2021-08-14 20:08:01 +0300

    main.go added

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/Evencaster/to-do-app-golang/controllers"
+	"github.com/Evencaster/to-do-app-golang/repository"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	repo := repository.New()
+	taskController := controllers.NewTaskController(repo)
+	r.GET("/", taskController.GetAllTasks)
+	r.POST("/", taskController.AddTask)
+	r.DELETE("/:id", taskController.RemoveTask)
+	r.DELETE("/", taskController.RemoveAllTasks)
+	err := r.Run("127.0.0.1:4444")
+	if err != nil {
+		panic(err)
+	}
+}
