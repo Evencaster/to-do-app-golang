1ab9197aa02079270799c195d9d0b6aa7211838a
Alexey NIkipelau <iloveyoucreator@gmail.com>
2021-08-16 21:47:43 +0300

    Serving static files added

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"github.com/Evencaster/to-do-app-golang/controllers"
 	"github.com/Evencaster/to-do-app-golang/repository"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.Static("/webapp", "./frontend/build")
 	repo := repository.NewMemRepo()
 	taskController := controllers.NewTaskController(repo)
-	r.GET("/", taskController.GetAllTasks)
-	r.POST("/", taskController.AddTask)
-	r.DELETE("/:id", taskController.RemoveTask)
-	r.DELETE("/", taskController.RemoveAllTasks)
-	err := r.Run("127.0.0.1:4444")
+
+	r.GET("/tasks", taskController.GetAllTasks)
+	r.POST("/tasks", taskController.AddTask)
+	r.DELETE("/tasks/:id", taskController.RemoveTask)
+	r.DELETE("/tasks", taskController.RemoveAllTasks)
+
+	r.Use(static.Serve("/", static.LocalFile("./frontend/build", false)))
+
+	err := r.Run("127.0.0.1:5555")
 	if err != nil {
 		panic(err)
 	}
